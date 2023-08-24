25677672d1fedaed9bf00a2a77e93bc8314a0970
João Madeira <joaomadeira1208@gmail.com>
2023-08-24 13:22:57 -0300

    24-08-2023

---
+++ AEDS2/Verde/TP1/Questao01.java
@@ -0,0 +1,31 @@
+import mypackage.MyIO;
+
+public class Questao01 {
+    public static boolean palindromo(String str) {
+        int j = str.length() - 1;
+        boolean isPalindromo = true;
+        for(int i = 0; i < str.length()/2; i++) {
+            if(str.charAt(i) != str.charAt(j)) {
+                isPalindromo = false;
+                i = str.length();
+            }
+            j--;
+        }
+        return isPalindromo;
+    }
+
+    public static void main(String[] args) {
+        String str;
+        do {
+            str = MyIO.readLine();
+            boolean isPalindromo = palindromo(str);
+            if(isPalindromo) {
+                MyIO.println("SIM");
+            }
+            else {
+                MyIO.println("NAO");
+            }
+        }while(str != "FIM");
+    }
+
+}
