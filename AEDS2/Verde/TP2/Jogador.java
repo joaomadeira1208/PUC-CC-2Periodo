b77d6eaa6eb9ae6774efcaa9d2abc7d130c5827b
João Madeira <joaomadeira1208@gmail.com>
2023-09-27 22:49:11 -0300

    27-09-2023

--- AEDS2/Verde/TP2/Jogador.java
+++ AEDS2/Verde/TP2/Jogador.java
@@ -1,4 +1,10 @@
-package Verde.TP2;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import mypackage.MyIO;
 
 public class Jogador {
     private int id;
@@ -97,8 +103,67 @@ public class Jogador {
         this.estadoNascimento = estadoNascimento;
     }
 
-    public static void main(String args[] {
-        
-    })
+    public Jogador clone() {
+        return new Jogador(id, nome, altura, peso, universidade, anoNascimento, cidadeNascimento, estadoNascimento);
+    }
+
+    public void imprimir() {
+        MyIO.print("[" + id + " ## " + nome + " ## " + altura + " ## " + peso + " ## " + universidade + " ## "
+                + anoNascimento + " ## " + cidadeNascimento + " ## " + estadoNascimento + "]");
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, Jogador> jogadores = new HashMap<>();
+
+        // Lê o arquivo CSV e cria instâncias de Jogador
+        try (BufferedReader br = new BufferedReader(new FileReader("players.csv"))) {
+            String linha;
+            boolean primeiraLinha = true; // Flag para verificar se é a primeira linha
+            while ((linha = br.readLine()) != null) {
+                if (primeiraLinha) {
+                    primeiraLinha = false; // Ignora a primeira linha (cabeçalho)
+                    continue;
+                }
+                String[] dados = linha.split(",");
+                if (dados.length >= 8) {
+                    int id = Integer.parseInt(dados[0]);
+                    String nome = dados[1];
+                    int altura = Integer.parseInt(dados[2]);
+                    int peso = Integer.parseInt(dados[3]);
+                    String universidade = dados[4];
+                    int anoNascimento = Integer.parseInt(dados[5]);
+                    String cidadeNascimento = dados[6];
+                    String estadoNascimento = dados[7];
+
+                    Jogador jogador = new Jogador(id, nome, altura, peso, universidade,
+                            anoNascimento, cidadeNascimento, estadoNascimento);
+
+                    jogadores.put(id, jogador);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Leitura dos IDs e impressão dos jogadores
+        Scanner scanner = new Scanner(System.in);
+        String linha;
+
+        while (true) {
+            linha = scanner.nextLine();
+            if (linha.equals("FIM")) {
+                break;
+            }
+
+            int id = Integer.parseInt(linha);
+
+            if (jogadores.containsKey(id)) {
+                Jogador jogador = jogadores.get(id);
+                jogador.imprimir();
+            } else {
+                System.out.println("Jogador com ID " + id + " não encontrado.");
+            }
+        }
+    }
 
 }
