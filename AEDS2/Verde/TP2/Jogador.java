c133c424f56364b086d3e0faeb1112e1c186ff2c
João Madeira <joaomadeira1208@gmail.com>
2023-09-27 22:09:46 -0300

    27-09-2023

---
+++ AEDS2/Verde/TP2/Jogador.java
@@ -0,0 +1,104 @@
+package Verde.TP2;
+
+public class Jogador {
+    private int id;
+    private String nome;
+    private int altura;
+    private int peso;
+    private String universidade;
+    private int anoNascimento;
+    private String cidadeNascimento;
+    private String estadoNascimento;
+
+    public Jogador() {
+        this.id = 0;
+        this.nome = "";
+        this.altura = 0;
+        this.peso = 0;
+        this.universidade = "";
+        this.anoNascimento = 0;
+        this.cidadeNascimento = "";
+        this.estadoNascimento = "";
+    }
+
+    public Jogador(int id, String nome, int altura, int peso, String universidade, int anoNascimento,
+            String cidadeNascimento, String estadoNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.altura = altura;
+        this.peso = peso;
+        this.universidade = universidade;
+        this.anoNascimento = anoNascimento;
+        this.cidadeNascimento = cidadeNascimento;
+        this.estadoNascimento = estadoNascimento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getUniversidade() {
+        return universidade;
+    }
+
+    public void setUniversidade(String universidade) {
+        this.universidade = universidade;
+    }
+
+    public int getAnoNasc() {
+        return anoNascimento;
+    }
+
+    public void setAnoNasc(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public String getCidadeNasc() {
+        return cidadeNascimento;
+    }
+
+    public void setCidadeNasc(String cidadeNascimento) {
+        this.cidadeNascimento = cidadeNascimento;
+    }
+
+    public String getEstadoNasc() {
+        return estadoNascimento;
+    }
+
+    public void setEstadoNasc(String estadoNascimento) {
+        this.estadoNascimento = estadoNascimento;
+    }
+
+    public static void main(String args[] {
+        
+    })
+
+}
